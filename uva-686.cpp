7d36d0064a672ac6ce65b365eb6e76cdbed7aacc
Sabrina Boby <63877634+sabrina-boby@users.noreply.github.com>
2022-08-04 01:40:54 +0600

    Add files via upload

--- uva-686.cpp
+++ uva-686.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define max 1000000
+#define max 32770
 vector<bool>prime(max,1);
 void sieve()
 {
-    long int i,j,m=sqrt(max+1);
+    int i,j,m=sqrt(max+1);
     prime[0]=prime[1]=0;
 
     for(i=2; i<=m; i++)
@@ -21,15 +21,15 @@ void sieve()
 int main()
 {
     sieve();
-    long int n;
-    while(scanf("%ld",&n)==1)
+    int n;
+    while(scanf("%d",&n)==1)
     {
-        long int i,m,x,flag=0;
+        int i,m,x,count=0;
         if(n==0)
         {
             break;
         }
-        m=n/2+1;
+        m=n/2;
         for(i=1; i<=m; i++)
         {
             if(prime[i])
@@ -37,15 +37,11 @@ int main()
                 x=n-i;
                 if(prime[x])
                 {
-                    flag=1;
-                    cout<<n<<" = "<<i<<" + "<<x<<endl;
-                    break;
+                    count++;
                 }
             }
         }
-        if(flag==0)
-        {
-            cout<<"Goldbach's conjecture is wrong."<<endl;
-        }
+        cout<<count<<endl;
     }
 }
+
